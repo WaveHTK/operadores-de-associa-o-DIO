d77e921ba7a12ed9f7e41d3bee9f40ecead89b9d
WaveHTK <149261124+WaveHTK@users.noreply.github.com>
2024-04-25 14:48:30 -0300

    Add files via upload

---
+++ operadores de associação.py
@@ -0,0 +1,14 @@
+Frutas = ("maçã", "banana", "Limão")
+Materias = ("Matemática", "Portugês","Geografia", "História", "Artes")
+
+print("maçã" in Frutas)
+print("Limão" in Frutas)
+print("Cereja" not in Frutas)
+print("banana" in Frutas)
+print("Limão" not in Frutas)
+
+print("Matemática" in Materias)
+print("Física" in Materias)
+print("Educação Física" not in Materias)
+print("Biologia" in Materias)
+print("Inglês" not in Materias)
